701f5895d7da9b1e36070513b7e7037e6550a5db
hackforum1988-code <hackforum1988@gmail.com>
2026-02-05 21:13:50 +0100

    Modify asset prices and add new assets
    Updated prices and attributes for existing assets and added new assets.

--- AssetCatalog.lua
+++ AssetCatalog.lua
@@ -1,9 +1,18 @@
 -- AssetCatalog.lua (ServerScriptService)
 return {
-	["Boneca Ambalabu"]    = { TemplateName = "Boneca Ambalabu",    Price = 10,  MiningPower = 2,  IncomePerSec = 2,  Rarity = "Uncommon"  },
-	["Brr Brr Patapim"]    = { TemplateName = "Brr Brr Patapim",    Price = 5,   MiningPower = 1,  IncomePerSec = 1,  Rarity = "Common"    },
-	["Cappuccino Assassino"]= { TemplateName = "Cappuccino Assassino", Price = 20, MiningPower = 4,  IncomePerSec = 3,  Rarity = "Uncommon" },
-	["Carloo"]             = { TemplateName = "Carloo",             Price = 100, MiningPower = 25, IncomePerSec = 20, Rarity = "Mythic"    },
-	["Cocofanto Elefanto"] = { TemplateName = "Cocofanto Elefanto", Price = 35,  MiningPower = 7,  IncomePerSec = 3,  Rarity = "Rare"      },
-	["Crabbo Limonetta"]   = { TemplateName = "Crabbo Limonetta",   Price = 60,  MiningPower = 12, IncomePerSec = 6,  Rarity = "Legendary" },
+	["Boneca Ambalabu"]      = { TemplateName = "Boneca Ambalabu",      Price = 12,  MiningPower = 2,  IncomePerSec = 2,  Rarity = "Uncommon"  },
+	["Brr Brr Patapim"]      = { TemplateName = "Brr Brr Patapim",      Price = 5,   MiningPower = 1,  IncomePerSec = 1,  Rarity = "Common"    },
+	["Cappuccino Assassino"] = { TemplateName = "Cappuccino Assassino", Price = 22,  MiningPower = 4,  IncomePerSec = 3,  Rarity = "Uncommon"  },
+	["Carloo"]               = { TemplateName = "Carloo",               Price = 110, MiningPower = 26, IncomePerSec = 21, Rarity = "Mythic"    },
+	["Cocofanto Elefanto"]   = { TemplateName = "Cocofanto Elefanto",   Price = 40,  MiningPower = 7,  IncomePerSec = 3,  Rarity = "Rare"      },
+	["Crabbo Limonetta"]     = { TemplateName = "Crabbo Limonetta",     Price = 65,  MiningPower = 12, IncomePerSec = 6,  Rarity = "Legendary" },
+
+	["67"]                   = { TemplateName = "67",                   Price = 45,  MiningPower = 8,  IncomePerSec = 4,  Rarity = "Rare"      },
+	["Frigo Camelo"]         = { TemplateName = "Frigo Camelo",         Price = 18,  MiningPower = 3,  IncomePerSec = 2,  Rarity = "Uncommon"  },
+	["Gangster Footera"]     = { TemplateName = "Gangster Footera",     Price = 75,  MiningPower = 13, IncomePerSec = 7,  Rarity = "Legendary" },
+	["Job Job Job Sahur"]    = { TemplateName = "Job Job Job Sahur",    Price = 42,  MiningPower = 7,  IncomePerSec = 4,  Rarity = "Rare"      },
+	["Orangutini Ananassini"]= { TemplateName = "Orangutini Ananassini",Price = 48,  MiningPower = 9,  IncomePerSec = 5,  Rarity = "Rare"      },
+	["Pipi Kiwi"]            = { TemplateName = "Pipi Kiwi",            Price = 6,   MiningPower = 1,  IncomePerSec = 1,  Rarity = "Common"    },
+	["Sigma Boy"]            = { TemplateName = "Sigma Boy",            Price = 120, MiningPower = 28, IncomePerSec = 23, Rarity = "Mythic"    },
+	["Tim Cheese"]           = { TemplateName = "Tim Cheese",           Price = 18,  MiningPower = 3,  IncomePerSec = 2,  Rarity = "Uncommon"  },
 }
